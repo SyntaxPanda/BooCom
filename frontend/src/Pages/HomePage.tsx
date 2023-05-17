785e2439022f053e7943669cb8d8b9f86d363f4c
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-17 15:07:37 +0200

    create RegisterPage, add RegisterPage to HomePage with Router and add HomePage to App with Router

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import RegisterPage from "./RegisterPage";
 
 export default function HomePage() {
     return (
-        <div></div>
+        <div>
+            <div>
+                <header>
+                    <Routes>
+                        <Route>
+                            <RegisterPage/>
+                        </Route>
+                    </Routes>
+                </header>
+            </div>
+        </div>
     );
 }
 
