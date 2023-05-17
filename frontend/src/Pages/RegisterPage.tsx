4505db11c9c6034fad47b9215eede4a736a779a4
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-17 18:01:37 +0200

    adding select field, button, Handler, axios.post(addUser)

--- frontend/src/Pages/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import axios from "axios";
+import {User} from "../Types/UserType";
 
-export default function RegisterPage() {
+export default function RegisterPage(props: User) {
+    const [name, setName] =
+        useState<string>("")
+    const [password, setPassword] =
+        useState<string>("")
 
+
+    const [userList, setUserList] =
+        useState<User[]>([])
+
+    function addUser(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        axios.post("/api/register/addUser", {
+            name: name,
+            password: password,
+            course: "bo-java-23-1"
+        })
+            .then(response => setUserList(response.data))
+        setName("")
+        setPassword("")
+    }
+
+    function OnChangeHandlerUserName(event: ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value)
+    }
+
+    function OnChangeHandlerUserPassword(event: ChangeEvent<HTMLInputElement>) {
+        setPassword(event.target.value)
+    }
 
     return (
         <div>
-            <form>
+            <form onSubmit={addUser}>
                 <div>
-                    Name:
-                    <input placeholder="Name"  type="text"/>
+                    <input placeholder="Name" type="text" value={name} onChange={OnChangeHandlerUserName}/>
                 </div>
                 <div>
-                    Password:
-                    <input placeholder={"Password"} type="text"/>
+                    <input placeholder={"Password"} type="password" value={password}
+                           onChange={OnChangeHandlerUserPassword}/>
+                </div>
+                <div>
+                    <select>
+                        <option value="bo-java-23-1">Bo-java-23-1</option>
+                    </select>
                 </div>
                 <div>
                     <button>Send</button>
