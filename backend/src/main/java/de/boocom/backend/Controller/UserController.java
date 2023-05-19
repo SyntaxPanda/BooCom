a6ce4bf45a64ba55a5c71840320e7b30da94235e
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-19 10:32:34 +0200

    add UserController.class add packages in backend

---
+++ backend/src/main/java/de/boocom/backend/Controller/UserController.java
@@ -0,0 +1,13 @@
+package de.boocom.backend.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+
+}
