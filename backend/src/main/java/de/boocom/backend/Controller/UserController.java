3f3c6a6eedcd90769cbcb2c91077819e77743d81
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-19 11:52:41 +0200

    add method addUser in UserController.java

--- backend/src/main/java/de/boocom/backend/Controller/UserController.java
+++ backend/src/main/java/de/boocom/backend/Controller/UserController.java
@@ -1,7 +1,9 @@
 package de.boocom.backend.Controller;
 
+import de.boocom.backend.Model.User;
 import de.boocom.backend.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,8 @@ public class UserController {
 
     private final UserService userService;
 
-
+    @PostMapping ("/register/addUser")
+    public void addUser(User userToAdd){
+        userService.addUser(userToAdd);
+    }
 }
