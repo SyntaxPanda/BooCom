4fb58a0a1b78134a10447db925eeef0c07075b81
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-19 10:35:46 +0200

    add UserService.java add injection from UserService.java

--- backend/src/main/java/de/boocom/backend/Controller/UserController.java
+++ backend/src/main/java/de/boocom/backend/Controller/UserController.java
@@ -1,5 +1,6 @@
 package de.boocom.backend.Controller;
 
+import de.boocom.backend.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
 
 }
---
+++ backend/src/main/java/de/boocom/backend/Service/UserService.java
@@ -0,0 +1,4 @@
+package de.boocom.backend.Service;
+
+public class UserService {
+}
