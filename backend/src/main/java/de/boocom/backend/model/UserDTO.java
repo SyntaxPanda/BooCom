af4bd8b4a4fcab1c9bccde4eb1b47b4ac75016e2
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-19 14:43:32 +0200

    add UserDTO

---
+++ backend/src/main/java/de/boocom/backend/model/UserDTO.java
@@ -0,0 +1,16 @@
+package de.boocom.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String name;
+    private String id;
+    private UserCourse course;
+    private String img;
+}
