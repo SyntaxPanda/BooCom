57eab2822184dd904bfcc2689947bef397cbe8ae
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-19 10:47:29 +0200

    add UserModel

---
+++ backend/src/main/java/de/boocom/backend/Model/User.java
@@ -0,0 +1,10 @@
+package de.boocom.backend.Model;
+
+public class User {
+
+    private String name;
+    private String password;
+    private String id;
+    private Enum course;
+    private String img;
+}
