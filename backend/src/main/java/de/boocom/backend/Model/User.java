99ba35357cdd993eb8eac08262de946f39560a21
johanneswittenburg <johanneswittenburg@gmail.com>
2023-05-19 11:58:57 +0200

    add All- & NoConstructor + @Data

--- backend/src/main/java/de/boocom/backend/Model/User.java
+++ backend/src/main/java/de/boocom/backend/Model/User.java
@@ -1,5 +1,12 @@
 package de.boocom.backend.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private String name;
