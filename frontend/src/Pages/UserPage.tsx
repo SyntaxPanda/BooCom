397fef9a912377a479e4f67bfe6db2d7da925dcf
paffen <pavel.stoyanov0@hm.edu>
2023-05-23 16:55:46 +0200

    add User useState ,axios call(get)

--- frontend/src/Pages/UserPage.tsx
+++ frontend/src/Pages/UserPage.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {User} from "../Types/UserType";
+import axios from "axios";
 
 
 export default function UserPage(props: User) {
 
-
+    const[user , setUser] = useState<User>({course: "", password: "", name:"", id: "", img:""})
+    function getUserPageById(){
+        axios.get(`/user/${props.id}`)
+             .then( response => {
+                 setUser(response.data)
+            })
+    }
     return (
         <div>
             <div>Bild</div>
-            <div>Name</div>
+            <div>{user.name}</div>
         </div>
     );
 }
