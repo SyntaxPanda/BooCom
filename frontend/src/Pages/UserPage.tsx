c186083881b6c8468c8fbe610e8e78d740113cb5
paffenst <paffen.st@gmail.com>
2023-05-24 11:49:17 +0200

    add params id

--- frontend/src/Pages/UserPage.tsx
+++ frontend/src/Pages/UserPage.tsx
@@ -1,19 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {User} from "../Types/UserType";
 import axios from "axios";
+import {useParams} from "react-router-dom";
 
 export default function UserPage() {
 
     const[user , setUser] = useState<User>({course: "", password: "", name:"", id: "", img:""})
+    const params = useParams()
+    const id : string | undefined = params.id
     function getUserPageById(){
-
-        axios.get(`/user/${user.id}`)
+        axios.get("/api/user/" + id)
              .then( response => setUser(response.data)
             ).catch(error => console.error(error))
 
-
     }
-    useEffect(getUserPageById)
+    useEffect(getUserPageById,[])
     return (
         <div>
             <div>Bild</div>
