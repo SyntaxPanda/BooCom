0650ae29a02c218478f9b126612244c405f84987
paffenst <paffen.st@gmail.com>
2023-05-24 10:54:02 +0200

    add getUserPageById methode

--- backend/src/main/java/de/boocom/backend/service/UserService.java
+++ backend/src/main/java/de/boocom/backend/service/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
                 userToAdd.getImg()
                 );
     }
+
+    public User getUserPageById(String id){
+        return userRepository.getUserPageById(id);
+    }
 }
