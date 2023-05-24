d0dd2715f36354ae3f78940f790a89fb79edf2aa
paffenst <paffen.st@gmail.com>
2023-05-24 11:53:31 +0200

    hot fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,14 @@ import {Route, Routes} from "react-router-dom";
 import RegisterPage from  "./Pages/RegisterPage";
 import Header from  './Pages/Header';
 import UserPage from "./Pages/UserPage";
-import {User} from "./Types/UserType";
 
 function App() {
- const[user , setUser] = useState<User>()
+
   return (
     <div className="App">
         <header><Header/></header>
         <Routes>
-            <Route path={`/user/${user?.id}`} element={<UserPage/>}/>
+            <Route path={`/user/:id`} element={<UserPage/>}/>
             <Route path={"/register"} element={<RegisterPage course={""} id={""} img={""} name={""} password={""} />}/>
             <Route path={"/"} element={<HomePage/>}/>
         </Routes>
