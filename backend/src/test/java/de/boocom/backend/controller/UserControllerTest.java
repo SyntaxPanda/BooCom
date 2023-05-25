63a1d36d7aa7ce7b09114a1c0974a4e62367147f
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-25 15:55:41 +0200

    add integrationTest for getAllUser methode

--- backend/src/test/java/de/boocom/backend/controller/UserControllerTest.java
+++ backend/src/test/java/de/boocom/backend/controller/UserControllerTest.java
@@ -9,11 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -74,4 +72,13 @@ class UserControllerTest {
                         }"""
                 )).andExpect(jsonPath("$.id").value(user.getId()));
     }
+
+    @Test
+    @DirtiesContext
+    void getAllUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
 }
