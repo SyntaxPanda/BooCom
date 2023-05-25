61c8a7826d03b4bce4e93c94d673b52b2a70307a
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-25 15:32:27 +0200

    #BetaVersion create a searchbar to find User with his/her's names, use "form" to combine input with button use useEffect to use the getUserFromList function if filter will be changed

--- frontend/src/pages/Footer.tsx
+++ frontend/src/pages/Footer.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+import {User} from "../types/UserType";
 
 export default function Footer() {
+    const navigate = useNavigate();
+
+    const [userList, setUserList] =
+        useState<User[]>([])
+
+    const [filter, setFilter] =
+        useState("")
+
+    function getUserFromList() {
+        axios.get("/api/users")
+            .then(response => {
+                setUserList(response.data);
+
+                const filteredUser = userList.find(user =>
+                    user.name.toLowerCase().includes(filter.toLowerCase())
+                );
+
+                if (filteredUser) {
+                    navigate("/user/" + filteredUser.id);
+                }
+            })
+            .catch(error => {
+                console.log("Error fetching user data:", error);
+            });
+    }
+
+    useEffect(() => {
+        getUserFromList();
+    }, [filter]);
+
+    function onTextChange(e: ChangeEvent<HTMLInputElement>) {
+        setFilter(e.target.value)
+    }
+
     return (
         <div>
-
+            <form>
+                <div>
+                    <input type="text" onChange={onTextChange}/>
+                </div>
+                <div>
+                    <button type={"submit"}>Find</button>
+                </div>
+            </form>
         </div>
     );
 }
