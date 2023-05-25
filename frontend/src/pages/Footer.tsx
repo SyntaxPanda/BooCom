61b739a0c08a599a16e0211380152fbe696518ce
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-25 16:49:48 +0200

    changes to refresh UserPage

--- frontend/src/pages/Footer.tsx
+++ frontend/src/pages/Footer.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
 import {User} from "../types/UserType";
@@ -16,36 +16,37 @@ export default function Footer() {
         axios.get("/api/users")
             .then(response => {
                 setUserList(response.data);
-
-                const filteredUser = userList.find(user =>
-                    user.name.toLowerCase().includes(filter.toLowerCase())
-                );
-
-                if (filteredUser) {
-                    navigate("/user/" + filteredUser.id);
-                }
             })
             .catch(error => {
                 console.log("Error fetching user data:", error);
             });
     }
 
-    useEffect(() => {
-        getUserFromList();
-    }, [filter]);
-
     function onTextChange(e: ChangeEvent<HTMLInputElement>) {
         setFilter(e.target.value)
     }
 
+    function onSubmitHandler(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const filteredUser = userList.find(user =>
+            user.name.toLowerCase().includes(filter.toLowerCase())
+        );
+
+        if (filteredUser) {
+            navigate("/user/" + filteredUser.id);
+        }
+    }
+
+    useEffect(getUserFromList, []);
+
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 <div>
                     <input type="text" onChange={onTextChange}/>
                 </div>
                 <div>
-                    <button type={"submit"}>Find</button>
+                    <button>Find</button>
                 </div>
             </form>
         </div>
