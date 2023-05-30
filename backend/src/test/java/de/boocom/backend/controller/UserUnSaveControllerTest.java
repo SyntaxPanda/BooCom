8f30c092ad67779ecc8c5b0eb757ee3b136e2d52
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-30 17:19:28 +0200

    add .with(csrf()) to all Post/Put/Delete test add @WithMockUser(username="username", password="password") to all test

--- backend/src/test/java/de/boocom/backend/controller/UserUnSaveControllerTest.java
+++ backend/src/test/java/de/boocom/backend/controller/UserUnSaveControllerTest.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -23,6 +25,7 @@ class UserUnSaveControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "Tim", password = "123")
     void addUser() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/register/user")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -32,7 +35,8 @@ class UserUnSaveControllerTest {
                                 "password": "123456",
                                 "course":"BOJAVA231"
                                 }"""
-                        ))
+                        )
+                        .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(content().json(
                         """
@@ -46,6 +50,7 @@ class UserUnSaveControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "Tim", password = "123")
     void getUserById() throws Exception {
         MvcResult response = mockMvc.perform(MockMvcRequestBuilders.post("/api/register/user")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -55,7 +60,8 @@ class UserUnSaveControllerTest {
                         "password": "123456",
                         "course":"BOJAVA231"
                         }"""
-                )).andReturn();
+                )
+                .with(csrf())).andReturn();
 
         String content = response.getResponse().getContentAsString();
 
@@ -75,6 +81,7 @@ class UserUnSaveControllerTest {
 
     @Test
     @DirtiesContext
+    @WithMockUser(username = "Tim", password = "123")
     void getAllUser() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/users"))
                 .andExpect(status().isOk())
