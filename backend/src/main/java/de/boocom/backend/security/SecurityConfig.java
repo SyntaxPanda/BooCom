66617b3d793973bab772c3a74438e4fc58f5953d
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-30 10:40:15 +0200

    add NoOpPasswordEncoder

--- backend/src/main/java/de/boocom/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/boocom/backend/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
@@ -18,5 +20,10 @@ public class SecurityConfig {
                         .password("123")
                         .build()
         );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
