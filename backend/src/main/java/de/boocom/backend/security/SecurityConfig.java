a62236998d0ad36e35fea95cfe58f0f40d1e4c97
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-30 10:38:46 +0200

    add InMemoryUserDetailsManager

--- backend/src/main/java/de/boocom/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/boocom/backend/security/SecurityConfig.java
@@ -1,11 +1,22 @@
 package de.boocom.backend.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("Tim")
+                        .password("123")
+                        .build()
+        );
+    }
 }
