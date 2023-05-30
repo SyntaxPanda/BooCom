d3e0f6ec561fa0772a800dbd005b9fd90e487ce7
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-30 10:34:42 +0200

    add annotation

--- backend/src/main/java/de/boocom/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/boocom/backend/security/SecurityConfig.java
@@ -1,4 +1,11 @@
 package de.boocom.backend.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
+
 }
