60f6ed3a3ec041119997df3bf7ceb29b3bd8a8ec
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-30 12:14:17 +0200

    Changed DTO file and delete document

--- backend/src/main/java/de/boocom/backend/model/UserDTO.java
+++ backend/src/main/java/de/boocom/backend/model/UserDTO.java
@@ -3,12 +3,10 @@ package de.boocom.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document("UsersDTO")
 public class UserDTO {
 
     private String name;
