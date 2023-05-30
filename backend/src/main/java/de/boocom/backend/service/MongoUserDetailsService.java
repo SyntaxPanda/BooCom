cf2ede011928509bb8bf1398696438b93ea14010
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-30 15:30:52 +0200

    create method for all ways to use mongodb as db

--- backend/src/main/java/de/boocom/backend/service/MongoUserDetailsService.java
+++ backend/src/main/java/de/boocom/backend/service/MongoUserDetailsService.java
@@ -1,5 +1,6 @@
 package de.boocom.backend.service;
 
+import de.boocom.backend.model.UserDTO;
 import de.boocom.backend.model.UserUnSave;
 import de.boocom.backend.repo.MongoUserRepo;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +10,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MongoUserDetailsService implements UserDetailsService {
     private final MongoUserRepo repo;
+    private final GenerateId generateUUID;
 
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
@@ -23,9 +27,29 @@ public class MongoUserDetailsService implements UserDetailsService {
         return new User(optionalUserUnSave.getName(), optionalUserUnSave.getPassword(), List.of());
     }
 
-    public void addUser(UserUnSave userUnSave) {
+    public UserDTO addUser(UserUnSave userUnSave) {
+        userUnSave.setId(generateUUID.generateId());
         repo.insert(userUnSave);
+        return new UserDTO(
+                userUnSave.getName(),
+                userUnSave.getId(),
+                userUnSave.getCourse(),
+                userUnSave.getImg()
+        );
     }
 
-    public
+    public UserDTO getUserPageById(String id) {
+        Optional<UserUnSave> userUnSave = repo.findById(id);
+        return userUnSave.get().convertUserToUserDTO();
+    }
+
+    public List<UserDTO> getAllUser() {
+        List<UserUnSave> userUnSaves = new ArrayList<>(repo.findAll());
+        List<UserDTO> userDTOS = new ArrayList<>();
+
+        for (UserUnSave userUnSave : userUnSaves) {
+            userDTOS.add(userUnSave.convertUserToUserDTO());
+        }
+        return userDTOS;
+    }
 }
