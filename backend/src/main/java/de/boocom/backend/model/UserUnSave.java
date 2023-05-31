8816f3722a8b48954e525f342755e7082c3da941
SyntaxPanda <rodetimy1996@yahoo.com>
2023-05-31 11:03:40 +0200

    dele id ano

--- backend/src/main/java/de/boocom/backend/model/UserUnSave.java
+++ backend/src/main/java/de/boocom/backend/model/UserUnSave.java
@@ -3,7 +3,6 @@ package de.boocom.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -14,7 +13,6 @@ public class UserUnSave {
 
     private String name;
     private String password;
-    @Id
     private String id;
     private UserCourse course;
     private String img;
