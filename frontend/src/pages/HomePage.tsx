f507afb06e64de7a9fa1082890103abed54e9aa6
johanneswittenburg <johanneswittenburg@gmail.com>
2023-06-01 13:26:58 +0200

    add NEW POST button with form dialogue to HomePage.tsx with mui library

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../css/headerCSS/HeaderPage.css"
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent, DialogContentText,
+    DialogTitle,
+    IconButton,
+    styled,
+    TextField,
+    Typography
+} from "@mui/material";
 
 export default function HomePage() {
 
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <div className={"registerPage"}>
-            Hi im the HomePage
+        <div>
+            <Button style={{backgroundColor: "gold", color: "black"}} variant="outlined" onClick={handleClickOpen}>
+                NEW POST
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>New Post</DialogTitle>
+                <DialogContent style={{
+                    width: 500,
+                    height: 100
+                }}
+                >
+                    <DialogContentText>
+                        User name
+                   </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="add new post"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={handleClose}>Post</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
