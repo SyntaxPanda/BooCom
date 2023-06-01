a0dfe5c2d48a9a4bceccb44b74fd01ca883ace0e
paffenst <paffen.st@gmail.com>
2023-06-01 11:07:28 +0200

    clear unused code

--- backend/src/main/java/de/boocom/backend/controller/UserController.java
+++ backend/src/main/java/de/boocom/backend/controller/UserController.java
@@ -3,12 +3,9 @@ package de.boocom.backend.controller;
 import de.boocom.backend.model.UserDTO;
 import de.boocom.backend.model.UserUnSave;
 import de.boocom.backend.service.MongoUserDetailsService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -17,22 +14,6 @@ import java.util.List;
 public class UserController {
 
     private final MongoUserDetailsService userService;
-
-    @GetMapping("/user/me")
-    public String getMe1(Principal principal) {
-        if (principal != null) {
-            return principal.getName();
-        }
-        return "AnonymousUser";
-    }
-
-    @GetMapping("/user/me2")
-    public String getMe2() {
-        return SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getName();
-    }
 
     @PostMapping("/register/user")
     public UserDTO addUser(@RequestBody UserUnSave userUnSaveToAdd) {
@@ -49,16 +30,9 @@ public class UserController {
         return userService.getAllUser();
     }
 
-
     @PostMapping("/login")
     void login() {
         SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
-    @PostMapping("/logout")
-    String logout(HttpSession httpSession) {
-        httpSession.invalidate();
-        SecurityContextHolder.clearContext();
-        return "logged out";
-    }
 }
