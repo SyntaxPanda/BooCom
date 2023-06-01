2ec662fa3514a47743dcf2b8d02a15259696671e
johanneswittenburg <johanneswittenburg@gmail.com>
2023-06-01 15:08:34 +0200

    update on min/maxRow within NEW POST form

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ import {
     DialogContent, DialogContentText,
     DialogTitle,
     IconButton,
-    styled,
+    styled, TextareaAutosize,
     TextField,
     Typography
 } from "@mui/material";
@@ -24,6 +24,59 @@ export default function HomePage() {
         setOpen(false);
     };
 
+    const blue = {
+        100: '#DAECFF',
+        200: '#b6daff',
+        400: '#3399FF',
+        500: '#007FFF',
+        600: '#0072E5',
+        900: '#003A75',
+    };
+
+    const grey = {
+        50: '#f6f8fa',
+        100: '#eaeef2',
+        200: '#d0d7de',
+        300: '#afb8c1',
+        400: '#8c959f',
+        500: '#6e7781',
+        600: '#57606a',
+        700: '#424a53',
+        800: '#32383f',
+        900: '#24292f',
+    };
+
+    const StyledTextarea = styled(TextareaAutosize)(
+        ({ theme }) => `
+    width: 460px;
+    height: 300px;
+    font-family: IBM Plex Sans, sans-serif;
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: 12px;
+    border-radius: 12px 12px 0px 12px;
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+  
+    &:hover {
+      border-color: ${blue[400]};
+    }
+  
+    &:focus {
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
+    }
+  
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `,
+    );
+
     return (
         <div>
             <Button style={{backgroundColor: "gold", color: "black"}} variant="outlined" onClick={handleClickOpen}>
@@ -33,20 +86,18 @@ export default function HomePage() {
                 <DialogTitle>New Post</DialogTitle>
                 <DialogContent style={{
                     width: 500,
-                    height: 100
+                    height: 500
                 }}
                 >
                     <DialogContentText>
                         User name
-                   </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="add new post"
-                        type="text"
-                        fullWidth
-                        variant="standard"
+                    </DialogContentText>
+                    <StyledTextarea
+                        maxRows={20}
+                        minRows={20}
+                        aria-label="maximum height"
+                        placeholder="Maximum 4 rows"
+                        defaultValue="blub here plz"
                     />
                 </DialogContent>
                 <DialogActions>
