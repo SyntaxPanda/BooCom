fbc65794fd9d30a87cfa0816054a4a6f4e5408e8
paffenst <paffen.st@gmail.com>
2023-06-01 11:11:29 +0200

    clear unused code

--- backend/src/main/java/de/boocom/backend/repo/MongoUserRepo.java
+++ backend/src/main/java/de/boocom/backend/repo/MongoUserRepo.java
@@ -3,7 +3,6 @@ package de.boocom.backend.repo;
 import de.boocom.backend.model.UserUnSave;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
