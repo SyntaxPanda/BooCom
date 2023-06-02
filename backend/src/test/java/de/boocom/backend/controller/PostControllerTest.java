6bda80fc9c0972817642548a4afbdf542c434582
paffenst <paffen.st@gmail.com>
2023-06-02 12:20:53 +0200

    test addPost function

---
+++ backend/src/test/java/de/boocom/backend/controller/PostControllerTest.java
@@ -0,0 +1,45 @@
+package de.boocom.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMVC;
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "Test", password = "123")
+    void addPost() throws Exception {
+        mockMVC.perform(MockMvcRequestBuilders.post("/api/post/new")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                                {
+                                "title":"te",
+                                "description": "123456",
+                                "userName":"test"
+                                }"""
+                ).with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        """
+                                {
+                                "title":"te",
+                                "description": "123456",
+                                "userName":"test"               
+                                }"""
+                )).andExpect(jsonPath("$.id").isNotEmpty());
+    }
+}
